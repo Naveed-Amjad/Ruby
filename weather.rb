c2ad5717f119cf666c0bca5945a00fc39022923d
Naveed-Amjad <naveedamjad1620@gmail.com>
2023-12-26 18:36:56 +0500

    file name read from command arguments

--- weather.rb
+++ weather.rb
@@ -19,51 +19,110 @@ require 'date'
 # # Display the filenames
 # puts filenames
 
-@address = "Dubai_weather/*.txt"
-filenames = Dir.glob(@address)
+# @address = "Dubai_weather/*.txt"
+# filenames = Dir.glob(@address)
 
 # Display the filenames
 # puts filenames
 
-tem_array = []
-min_array = []
+# tem_array = []
+# min_array = []
 
-filenames.each do |filename|
-    table = CSV.parse(File.read(filename), headers: true)
-    date_string = table[0][0];
-    parsed_date = Date.parse(date_string)
-    month_name = parsed_date.strftime('%B')
-    # Now you can work with the CSV data in the 'table' variable
-    # For example, you can access columns using headers:
-    max_temperatures = table.by_col[1].map(&:to_i)
-    min_tempratures = table.by_col[3].map(&:to_i)
-    sum = 0
-    min_sum = 0
-    max_temperatures.each do |temp|
-        sum = sum + temp
-    end
-    min_tempratures.each do |temp|
-        min_sum = min_sum + temp
-    end
-    puts "Average Max temprature in #{month_name} is #{sum/max_temperatures.length}"
-    puts "Average Min temprature in #{month_name} is #{min_sum/min_tempratures.length}"
+# filenames.each do |filename|
+#     table = CSV.parse(File.read(filename), headers: true)
+#     date_string = table[0][0];
+#     parsed_date = Date.parse(date_string)
+#     month_name = parsed_date.strftime('%B')
+#     # Now you can work with the CSV data in the 'table' variable
+#     # For example, you can access columns using headers:
+#     max_temperatures = table.by_col[1].map(&:to_i)
+#     min_tempratures = table.by_col[3].map(&:to_i)
+#     sum = 0
+#     min_sum = 0
+#     max_temperatures.each do |temp|
+#         sum = sum + temp
+#     end
+#     min_tempratures.each do |temp|
+#         min_sum = min_sum + temp
+#     end
+#     puts "Average Max temprature in #{month_name} is #{sum/max_temperatures.length}"
+#     puts "Average Min temprature in #{month_name} is #{min_sum/min_tempratures.length}"
 
-    max_temp = max_temperatures.max
-    min_temp = min_tempratures.min
-    tem_array.push(max_temp);
-    min_array.push(min_temp)
-    puts "Max temperature in #{filename}: #{max_temp} \n"
-    puts "Min temperature in #{filename}: #{min_temp} \n"
+#     max_temp = max_temperatures.max
+#     min_temp = min_tempratures.min
+#     tem_array.push(max_temp);
+#     min_array.push(min_temp)
+#     puts "Max temperature in #{filename}: #{max_temp} \n"
+#     puts "Min temperature in #{filename}: #{min_temp} \n"
 
-  end
+#   end
 
 # max = tem_array.max
-max, max_index = tem_array.each_with_index.max
+# max, max_index = tem_array.each_with_index.max
 
-min = min_array.min
+# min = min_array.min
 
-  puts "Temp Array = #{tem_array} \n\n"
-  puts "Max Temprature from all the files = #{max}\n"
+#   puts "Temp Array = #{tem_array} \n\n"
+#   puts "Max Temprature from all the files = #{max}\n"
 
-  puts "Min Array = #{min_array} \n\n"
-  puts "Min Temprature from all the files = #{min}\n"
+#   puts "Min Array = #{min_array} \n\n"
+#   puts "Min Temprature from all the files = #{min}\n"
+
+# Reading file names from command line
+
+@address = "Dubai_weather/Dubai_weather_#{ARGV[0]}"
+puts "address = #{@address}"
+filenames = Dir.glob(@address)
+
+filenames.each do |file_path|
+    # Extract the file name from the path
+    file_name = File.basename(file_path)
+  
+    # Split the file name using '/'
+    parts = file_name.split('/')
+  
+    # The last part of the split will be the desired part
+    desired_part = parts.last
+  
+    # Now you can use desired_part as needed
+    puts "Desired Part: #{desired_part}"
+
+    file_content = File.read(@address)
+
+    # Parse the content into a CSV table
+    csv_table = CSV.parse(file_content, headers: true)
+
+    # Now you can use csv_table as needed
+    puts "CSV Table for #{csv_table}:"
+
+    csv_table.each do |row|
+        # Access individual columns using column names
+        max_temp = row['Max TemperatureC'].to_i
+        min_temp = row['Min TemperatureC'].to_i
+        date = row['GST']
+        dateObj = Date.parse(date)
+        day = dateObj.day
+        if(max_temp && min_temp)
+            puts "Day = #{day}"
+        end
+        if max_temp
+            (1..max_temp).each do
+              print "+"
+            end
+            puts "#{max_temp}"
+        end
+        
+        if min_temp
+            (1..min_temp).each do
+              print "+"
+            end
+            puts "#{min_temp}"
+        end
+        puts " "
+        # puts "Max value = #{max_temp}"
+        # puts "Max value = #{min_temp}"
+        # puts "Max value = #{date}"
+    end
+
+    
+end
